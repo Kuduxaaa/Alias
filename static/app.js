1560ec477d287db48b12106da9f56a8f75585734
Kuduxaaa <68453819+Kuduxaaa@users.noreply.github.com>
2020-11-21 11:51:47 -0800

    Add files via upload

---
+++ static/app.js
@@ -0,0 +1,129 @@
+'use strict';
+var mStatus = document.querySelector('.status');
+var successButton = document.querySelector('.success_btn');
+var failedButton = document.querySelector('.failed_btn');
+var domTime = document.querySelector('.timeout');
+var domScore = document.querySelector('.score');
+var startButton = document.querySelector('.start_btn');
+var domGroup = document.querySelector('.groupName');
+var doc = document.documentElement;
+var countdownstarted = false;
+var time = 60;
+var score;
+var current_score = 0;
+var scores = {0:0,1:0};
+var randomWords = [
+  'ქურთუკი', 'ობობა', 'ნაცარი', 'ვირი', 'კედელი', 'ნარცისი', 'ღორი', 'ადამიანი', 'წინდა', 
+  'პარლამენტი', 'ბორბალი', "საიდუმლო", "წყალი", "ქალთევზა", "დრო", "ცეცხლი", "ცა", "მიწა",
+  "მშვილდისარი", "შარვალი", "ფანჯარა", "ტელევიზორი", 'გარემოსდაცვითი', 'მომაკვდინებელი', 
+  'დაგეგმვა', 'რჩევა', 'მხეცი', 'ნათესავი', 'ბაზარი', 'არსება', 'დაკავშირება', 'თემა', 'სტაბილურობა', 
+  'დემოკრატი', 'აღმოაჩინე', 'ბრძოლა', 'ფიზიკური', 'შემაჯამებელი', 'თვალები', 'პენსია', 'სკოლა', 
+  'კომპრომისი', 'ბლოკი', 'მცდელობა', 'მსახიობი', 'დაქირავება', 'იდაყვი', 'კალამი', 'ინტერნეტი',
+  'სამწვადე', 'კაცი', 'საჯაროობა', 'ტრაბახი', 'შეფასება', 'შეკრება', 'გამბედაობა', 'უზარმაზარი', 
+  'კონსულტაცია', 'უზრუნველყოფა', 'ძილი', 'მეგობარი', 'უბედურება', 'მოზარდი', 'რეგიონი', 
+  'ფირფიტა', 'კომპლექსი', 'გაგზავნა', 'მოძრაობა', 'ბეჭდვა', 'ტელეფონი', 'საყვარელი', 'იმედგაცრუება',
+  'ვარსკვლავი', 'ნდობა', 'საბაბი', 'ბოდიში', 'თანამემამულე', 'სროლა', 'დგომა', 'მოულოდნელი', 
+  'ეფექტური', 'ღობე', 'ფული', 'ყავა', 'ნაყინი', 'ცხენი', 'ყიდვა', 'ბუშტი', 'მგელი', 'ბალახი', 'მეუღლე',
+  'უთო', 'დენი', 'საწოლი', 'მთა' 
+];
+
+
+function back() {
+  return 'hash_changed :D';
+}
+
+var group_one = prompt('მოუთითეთ პირველი ჯგუფის სახელი: ');
+var group_two = prompt('მიუთითეთ მეორე ჯგუფის სახელი');
+var first_group = group_one;
+domGroup.innerText = 'ჯგუფი: ' + first_group;
+
+function openFullscreen(){
+  if (doc.requestFullscreen){
+    doc.requestFullscreen();
+  } else if (doc.webkitRequestFullscreen){
+    doc.webkitRequestFullscreen();
+  } else if (doc.msRequestFullscreen){
+    doc.msRequestFullscreen();
+  }
+}
+
+function startCountdown(){
+  countdownstarted = true;
+  var countDown = setInterval(function(){
+    time--;
+    var myAudio = new Audio('./static/timeout.mp3');
+    domTime.innerText = time + 's';
+    if (time <= 5 && time >= 1){
+      myAudio.play();
+    } 
+    if (time === 0){
+      time = 60;
+      current_score = 0;
+      if (first_group === group_one){
+        first_group = group_two;
+        score = scores[1];
+      } else {
+        first_group = group_one;
+        score = scores[0];
+      }
+      
+      domGroup.innerText = 'ჯგუფი: ' + first_group;
+      domScore.innerText = 'ქულა; ' + score;
+      countdownstarted = false;
+      startButton.style.display = "block";
+      successButton.style.display = "none";
+      failedButton.style.display = "none";
+      clearInterval(countDown);
+    }
+  }, 1000);
+}
+
+function addScore(sc){
+  if (first_group === group_one){
+    scores[0] = sc;
+  } else {
+    scores[1] = sc;
+  }
+}
+
+successButton.addEventListener('click', function(){
+  if (randomWords.length != 0){
+    var randomIndex = Math.floor(Math.random() * randomWords.length);
+    mStatus.innerText = randomWords[randomIndex];
+    randomWords.splice(randomIndex,1);
+  } else {
+    mStatus.innerText = "სიტყვები ამოიწურა :))";
+  }
+  new Audio('./static/success.mp3').play();
+  if (first_group === group_one){  
+    var index = 0;
+  } else {
+    var index = 1;
+  }
+  scores[index]++;
+  domScore.innerText = 'ქულა: ' + scores[index];
+  addScore(scores[index]);
+
+});
+
+
+failedButton.addEventListener('click', function(){
+  if (randomWords.length != 0){
+    var randomIndex = Math.floor(Math.random() * randomWords.length);
+    mStatus.innerText = randomWords[randomIndex];
+    randomWords.splice(randomIndex,1);
+  } else {
+    mStatus.innerText = "სიტყვები ამოიწურა :))";
+  }
+  new Audio('./static/click.mp3').play();
+});
+
+startButton.addEventListener('click', function(){
+  openFullscreen();
+  startButton.style.display = "none";
+  successButton.style.display = "block";
+  failedButton.style.display = "block";
+  if (!countdownstarted){
+    startCountdown();
+  }
+});
